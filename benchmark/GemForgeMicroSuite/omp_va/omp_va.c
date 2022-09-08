036ec18ac6b44216732f182bfe6a10497f6be7fb
k16diablo <miguel92@naver.com>
2022-09-08 16:54:03 +0900

    Specify OpenMP chunk to make compiler recognize streams

--- benchmark/GemForgeMicroSuite/omp_va/omp_va.c
+++ benchmark/GemForgeMicroSuite/omp_va/omp_va.c
@@ -1,10 +1,5 @@
-/**
-* @file app.c
-* @brief Template for a Host Application Source File.
-*
-*/
 #ifdef GEM_FORGE
-#include "gem5/m5ops.h"
+#include "../gfm_utils.h"
 #endif
 
 #include <stdio.h>
@@ -21,159 +16,70 @@
 
 typedef uint32_t Value;
 
-#define WARM_CACHE
-#define CACHE_BLOCK_SIZE 64
+// Turn off Cache warm-up by default, because it is not our assumption
+//#define WARM_CACHE
 
-static Value *A;
-static Value *B;
-static Value *C;
-static Value *C2; 
+//static const uint64_t file_size = 65536; 
+static const uint64_t file_size = 33554432; 
+//static const uint64_t file_size = 16777216;
 
-/**
-* @brief creates a "test file" by filling a buffer of 64MB with pseudo-random values
-* @param nr_elements how many 32-bit elements we want the file to be
-* @return the buffer address
-*/
-void  *create_test_file(uint64_t nr_elements) {
-    srand(0);
-    A = (Value*) aligned_alloc(CACHE_BLOCK_SIZE, nr_elements * sizeof(Value));
-    B = (Value*) aligned_alloc(CACHE_BLOCK_SIZE, nr_elements * sizeof(Value));
-    C = (Value*) aligned_alloc(CACHE_BLOCK_SIZE, nr_elements * sizeof(Value));
-    
-//    for (uint64_t i = 0; i < nr_elements; i++) {
-//        A[i] = (int) (rand());
-//        B[i] = (int) (rand());
-//    }
-}
-
-/**
-* @brief compute output in the host
-*/
-static void vector_addition_host(uint64_t nr_elements, int t) {
-    omp_set_dynamic(0);
-    omp_set_num_threads(t);
-    omp_set_schedule(omp_sched_static, 0);
-    #pragma omp parallel for schedule(static)
-    for (uint64_t i = 0; i < nr_elements; i += 16) {
-      __m512 valA = _mm512_loadu_epi32(A + i);
-      __m512 valB = _mm512_loadu_epi32(B + i);
-      __m512 valC = _mm512_add_epi32(valA, valB);
+__attribute__((noinline)) Value vector_addition_host(Value* A, Value* B, Value* C, int numThreads) {
+  #pragma omp parallel for schedule(static, file_size / numThreads) firstprivate(A, B, C)
+    for (uint64_t i = 0; i < file_size; i += 16) {
+      __m512i valA = _mm512_loadu_epi32(A + i);
+      __m512i valB = _mm512_loadu_epi32(B + i);
+      __m512i valC = _mm512_add_epi32(valA, valB);
       _mm512_storeu_epi32(C + i, valC);
     }
-//    for (uint64_t i = 0; i < nr_elements; i++) {
-//        C[i] = A[i] + B[i];
-//    }
+
+    return 0;
 }
 
-// Params ---------------------------------------------------------------------
-typedef struct Params {
-    uint64_t   input_size;
-    int   n_warmup;
-    int   n_reps;
-    int   n_threads;
-}Params;
-
-void usage() {
-    fprintf(stderr,
-        "\nUsage:  ./program [options]"
-        "\n"
-        "\nGeneral options:"
-        "\n    -h        help"
-        "\n    -t <T>    # of threads (default=8)"
-        "\n    -w <W>    # of untimed warmup iterations (default=1)"
-        "\n    -e <E>    # of timed repetition iterations (default=3)"
-        "\n"
-        "\nBenchmark-specific options:"
-        "\n    -i <I>    input size (default=8M elements)"
-        "\n");
-}
-
-struct Params input_params(int argc, char **argv) {
-    struct Params p;
-    p.input_size    = 16777216;
-    p.n_warmup      = 1;
-    p.n_reps        = 3;
-    p.n_threads     = 1;
-
-    int opt;
-    while((opt = getopt(argc, argv, "hi:w:e:t:")) >= 0) {
-        switch(opt) {
-        case 'h':
-        usage();
-        exit(0);
-        break;
-        case 'i': p.input_size    = atoi(optarg); break;
-        case 'w': p.n_warmup      = atoi(optarg); break;
-        case 'e': p.n_reps        = atoi(optarg); break;
-        case 't': p.n_threads        = atoi(optarg); break;
-        default:
-            fprintf(stderr, "\nUnrecognized option!\n");
-            usage();
-            exit(0);
-        }
-    }
-    assert(p.n_threads > 0 && "Invalid # of ranks!");
-
-    return p;
-}
-
-/**
-* @brief Main of the Host Application.
-*/
 int main(int argc, char **argv) {
 
-//    struct Params p = input_params(argc, argv);
-//
-//    const uint64_t file_size = p.input_size;
+  int numThreads = 1;
+  if (argc == 2) {
+    numThreads = atoi(argv[1]);
+  }
+  printf("Number of Threads: %d.\n", numThreads);
+  omp_set_dynamic(0);
+  omp_set_num_threads(numThreads);
+  omp_set_schedule(omp_sched_static, 0);
 
-    int numThreads = 1;
-    if (argc == 2) {
-      numThreads = atoi(argv[1]);
-    }
-    printf("Number of Threads: %d.\n", numThreads);
-    const uint64_t file_size = 33554432; 
-//    const uint64_t file_size = 16777216;
+  // Create an input file with arbitrary data.
+  Value* A = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
+  Value* B = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
+  Value* C = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
 
 #ifdef GEM_FORGE
-  m5_detail_sim_start();
+  gf_detail_sim_start();
 #endif
-
-    // Create an input file with arbitrary data.
-    create_test_file(file_size);
 
 #ifdef WARM_CACHE
-    // This should warm up the cache.
-    for (uint64_t i = 0; i < file_size; i += CACHE_BLOCK_SIZE / sizeof(Value)) {
-      volatile Value x = A[i];
-    }
-    for (uint64_t i = 0; i < file_size; i += CACHE_BLOCK_SIZE / sizeof(Value)) {
-      volatile Value x = B[i];
-    }
-    for (uint64_t i = 0; i < file_size; i += CACHE_BLOCK_SIZE / sizeof(Value)) {
-      volatile Value y = C[i];
-    }
-    // Start the threads.
-#pragma omp parallel for schedule(static)
-    for (int tid = 0; tid < numThreads; ++tid) {
-      volatile Value x = A[tid];
-    }
+  WARM_UP_ARRAY(A, file_size);
+  WARM_UP_ARRAY(B, file_size);
+  WARM_UP_ARRAY(C, file_size);
+  // Initialize the threads.
+#pragma omp parallel for schedule(static) firstprivate(A)
+  for (int tid = 0; tid < numThreads; ++tid) {
+    volatile Value x = *A;
+  }
 #endif
 
 #ifdef GEM_FORGE
-  m5_reset_stats(0, 0);
+  gf_reset_stats();
 #endif
 
-//    vector_addition_host(file_size, p.n_threads);
-    vector_addition_host(file_size, numThreads);
-	
+  vector_addition_host(A, B, C, numThreads);
+
 #ifdef GEM_FORGE
-  m5_detail_sim_end();
+  gf_detail_sim_end();
   exit(0);
 #endif
 
-    free(A);
-    free(B);
-    free(C);
+  free(A);
+  free(B);
+  free(C);
 
-   return 0;
- }
+  return 0;
+}
