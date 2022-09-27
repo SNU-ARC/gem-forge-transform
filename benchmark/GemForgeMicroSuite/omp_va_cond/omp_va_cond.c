ba2a19eb4b5654fdf2be557aa687769d84924923
k16diablo <miguel92@naver.com>
2022-09-27 15:29:55 +0900

    add *visited, more similar to BiFS

--- benchmark/GemForgeMicroSuite/omp_va_cond/omp_va_cond.c
+++ benchmark/GemForgeMicroSuite/omp_va_cond/omp_va_cond.c
@@ -14,6 +14,8 @@
 #include <omp.h>
 #include "immintrin.h"
 
+//#define CHECK
+
 typedef uint32_t Value;
 
 // Turn off Cache warm-up by default, because it is not our assumption
@@ -22,16 +24,21 @@ typedef uint32_t Value;
 //static const uint64_t file_size = 65536; 
 //static const uint64_t file_size = 33554432; 
 //static const uint64_t file_size = 16777216;
-static const uint64_t file_size = 128 * 50000;
+static const uint64_t num_vector = 10000;
+static const uint64_t dim_vector = 256;
+static const uint64_t num_leaf = 50;
+static const uint64_t num_iter = 80;
+static const uint64_t file_size = num_vector * dim_vector;
 
-__attribute__((noinline)) Value vector_addition_host(Value* A, Value* B, Value* C, uint64_t* index_queue, int numThreads) {
+__attribute__((noinline)) Value vector_addition_host(Value* A, Value* B, Value* C, uint64_t* index_queue, uint32_t* visited, int numThreads) {
   #pragma omp parallel for schedule(static, file_size / numThreads) firstprivate(A, B, C)
-  for (uint64_t i = 0; i < 100; i++) {
+  for (uint64_t i = 0; i < num_leaf; i++) {
     uint64_t idx = *(index_queue + i);
-//    if (i % 4 == 0) continue;
-    for (uint64_t j = 0; j < 128; j++) {
+    if (visited[idx] == 0) continue;
+    for (uint64_t j = 0; j < dim_vector; j++) {
       C[idx + j] = A[idx + j] + B[idx + j];
     }
+    visited[idx] = true;
   }
   return 0;
 }
@@ -52,9 +59,12 @@ int main(int argc, char **argv) {
   Value* A = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
   Value* B = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
   Value* C = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
-  uint64_t* index_queue = (uint64_t*) aligned_alloc(CACHE_LINE_SIZE, 100 * sizeof(uint64_t));
-  for (uint64_t i = 0; i < 100; i++)
-    index_queue[i] = (rand() / 50000 - 1) << 7;
+  uint64_t* index_queue = (uint64_t*) aligned_alloc(CACHE_LINE_SIZE, num_iter * num_leaf * sizeof(uint64_t));
+  for (uint64_t i = 0; i < num_iter * num_leaf; i++)
+    index_queue[i] = rand() / num_vector;
+  uint32_t* visited = (uint32_t*) malloc(file_size * sizeof(uint32_t));
+  for (uint64_t i = 0; i < file_size; i++) 
+    visited[i] = i % 4;
 
 #ifdef GEM_FORGE
   gf_detail_sim_start();
@@ -75,7 +85,20 @@ int main(int argc, char **argv) {
   gf_reset_stats();
 #endif
 
-  vector_addition_host(A, B, C, index_queue, numThreads);
+  for (uint64_t i = 0; i < 5; i++) {
+    vector_addition_host(A, B, C, index_queue, visited, numThreads);
+    index_queue += num_leaf;
+  }
+
+#ifdef CHECK
+  uint64_t err_cnt = 0;
+  Value* C_CHECK = (Value*) aligned_alloc(CACHE_LINE_SIZE, file_size * sizeof(Value));
+  vector_addition_host(A, B, C_CHECK, index_queue, numThreads);
+  for (uint64_t i = 0; i < file_size; i++) {
+    err_cnt += (C[i] == C_CHECK[i]);
+  }
+  printf("Error count = %ld\n", err_cnt);
+#endif
 
 #ifdef GEM_FORGE
   gf_detail_sim_end();
@@ -85,6 +108,8 @@ int main(int argc, char **argv) {
   free(A);
   free(B);
   free(C);
+  free(index_queue);
+  free(visited);
 
   return 0;
 }
