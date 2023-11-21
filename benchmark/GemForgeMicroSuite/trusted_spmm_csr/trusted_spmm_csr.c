ba0af6d8260a35df90f0819092b4c231f2620cc4
Youngook-Song <songyu87@snu.ac.kr>
2023-11-21 14:21:11 +0900

    update trusted_spmm_csr.c

--- benchmark/GemForgeMicroSuite/trusted_spmm_csr/trusted_spmm_csr.c
+++ benchmark/GemForgeMicroSuite/trusted_spmm_csr/trusted_spmm_csr.c
@@ -10,24 +10,21 @@
 #include <getopt.h>
 #include <assert.h>
 #include <stdint.h>
+#include <time.h>
+
 
 #include <omp.h>
 #include "immintrin.h"
 
+
 // yosong, 231016
 #define INDEXTYPE uint64_t
 #define VALUETYPE float
-static const uint64_t num_node			  = 15126;
-static const uint64_t num_edge_hvd        = 824617; 
-static const uint64_t file_size_indx_hvd  = 2*num_edge_hvd;
-static const uint64_t file_size_pntr_hvd  = num_node;
-static const uint64_t dim_vector_hvd      = 128;
-static const uint64_t nonzero             = 1649234;
 static const uint64_t num_iter			  = 1;
 //#define CHECK
 
-#define PTTIME
-#define PSP
+//#define PTTIME
+//#define PSP
 
 typedef float Value;
 
@@ -118,6 +115,7 @@ __attribute__((noinline)) Value trusted_spmm_csr
 }
 
 int main(int argc, char **argv) {
+ 
   int numThreads = 1;
   printf("argc: %d, argv[1]: %s, argv[2]: %s\n", argc, argv[1], argv[2]);
   if (argc > 1) {
@@ -129,44 +127,131 @@ int main(int argc, char **argv) {
   omp_set_schedule(omp_sched_static, 0);
   srand(0);
 
+  char *ptr = NULL;
+  char dataset_path[256] = "";
+  char dataset_name[256] = "";
+  char filename[100];
+  char input_path[256] = "";
+
+  uint64_t nonzero;
+  uint64_t sz;
+  uint64_t num_node;
+  uint64_t total_num_node;
+  uint64_t num_dim;
+
+  strcpy(input_path,argv[2]);
+  ptr = strrchr(input_path, '/');
+  if(ptr==NULL){
+  	strcpy(dataset_name,input_path);
+  }
+  else{
+	strcpy(dataset_name,ptr+1);
+  }
+  //printf("dataset_name = %s\n",dataset_name); 
+  strcpy(dataset_path, input_path);
+  //printf("dataset_path = %s\n", dataset_path);
+  strcat(dataset_path, "/");
+  printf("dataset_path = %s\n", dataset_path);
+  strcat(dataset_path, dataset_name);
+
+  // ===============================================================================//
   // indx from file
-  FILE* fp_mtx = fopen("../dataset/graph/harvard/harvard_rows.dat", "rb");
-  INDEXTYPE* indx = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  file_size_indx_hvd * sizeof(INDEXTYPE));
+  strcpy(filename, dataset_path);
+  strcat(filename, "_rows.dat");
+  printf("file name = %s\n", filename);
+
+  FILE* fp_mtx = fopen(filename, "rb");
   if (fp_mtx != NULL) {
     fseek(fp_mtx, 0L, SEEK_END);
-    uint64_t sz = ftell(fp_mtx);
+    sz = ftell(fp_mtx);
     fseek(fp_mtx, 0L, SEEK_SET);
-    if (sz == file_size_indx_hvd * sizeof(INDEXTYPE)) {
-      fread((void*)indx, sizeof(INDEXTYPE), file_size_indx_hvd, fp_mtx);
-    }
-    fclose(fp_mtx);
+	fread((void*)&total_num_node, sizeof(INDEXTYPE), 1, fp_mtx);	
+	fread((void*)&nonzero, sizeof(INDEXTYPE), 1, fp_mtx);	
   }
   else {
-    printf("Cannot find harvard_row.dat\n");
+    printf("Cannot find %s\n", filename);
     return 0;
   }
+
+  //printf("nonzero = %d, total_num_node = %d\n",nonzero, total_num_node);
+  INDEXTYPE* indx = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  nonzero * sizeof(INDEXTYPE));
+
+  if (sz == (nonzero+2) * sizeof(INDEXTYPE)) {
+    fread((void*)indx, sizeof(INDEXTYPE), nonzero, fp_mtx);
+  }
+  else {
+	  printf("size of file(%s) is wrong\n", filename);
+	  return 0;
+  }
+  fclose(fp_mtx);
 
   //printf("indx[0] = %lu\n" ,indx[0]);
   //printf("indx[1] = %lu\n" ,indx[1]);
   //printf("indx[2] = %lu\n" ,indx[2]);
   //printf("indx[3] = %lu\n" ,indx[3]);
+  // ===============================================================================//
 
-  // pntrb from file
-  FILE* fp_mtx2 = fopen("../dataset/graph/harvard/harvard_pntrb.dat", "rb");
 
-  INDEXTYPE* pntrb = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  file_size_pntr_hvd * sizeof(INDEXTYPE));
-
+  // ===============================================================================//
+  // b from file
+  strcpy(filename, dataset_path);
+  strcat(filename, "_b_mat.dat");
+  printf("file name = %s\n", filename);
+  FILE* fp_mtx2 = fopen(filename, "rb");
   if (fp_mtx2 != NULL) {
     fseek(fp_mtx2, 0L, SEEK_END);
-    uint64_t sz = ftell(fp_mtx2);
+    sz = ftell(fp_mtx2);	
     fseek(fp_mtx2, 0L, SEEK_SET);
-    if (sz == file_size_pntr_hvd * sizeof(INDEXTYPE)) {
-      fread((void*)pntrb, sizeof(INDEXTYPE), file_size_pntr_hvd, fp_mtx2);
-    }
-    fclose(fp_mtx2);
+	fread((void*)&num_dim, sizeof(uint64_t), 1, fp_mtx2);	
+    //printf("sz = %d, num_dim = %d\n", sz, num_dim);
   }
   else {
-    printf("Cannot find harvard_pntrb.dat\n");
+    printf("Cannot find %s\n", filename);
+    return 0;
+  }
+
+  num_node = (sz-sizeof(uint64_t))/(sizeof(VALUETYPE)*num_dim);
+  //printf("sz = %d, num_dim = %d, num_node = %d\n", sz, num_dim, num_node);
+  VALUETYPE* b = (VALUETYPE*) aligned_alloc(CACHE_LINE_SIZE,  num_node*num_dim * sizeof(VALUETYPE));
+  INDEXTYPE ldb = num_dim;
+  if (sz == (num_node*num_dim) * sizeof(VALUETYPE) + sizeof(uint64_t)) {
+    fread((void*)b, sizeof(VALUETYPE), num_node*num_dim, fp_mtx2);
+  }
+  else {
+      printf("size of file(%s) is wrong\n", filename);
+      return 0;
+  }
+  fclose(fp_mtx2);
+
+  //printf("b[0] = %f\n" ,b[0]);
+  //printf("b[1] = %f\n" ,b[1]);
+  //printf("b[2] = %f\n" ,b[2]);
+  //printf("b[3] = %f\n" ,b[3]);
+  // ===============================================================================//
+
+  // ===============================================================================//
+  // pntrb from file
+  strcpy(filename, dataset_path);
+  strcat(filename, "_pntrb.dat");
+  printf("file name = %s\n", filename);
+  FILE* fp_mtx3 = fopen(filename, "rb");
+  INDEXTYPE* pntrb = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  total_num_node * sizeof(INDEXTYPE));
+
+  if (fp_mtx3 != NULL) {
+    fseek(fp_mtx3, 0L, SEEK_END);
+    uint64_t sz = ftell(fp_mtx3);
+    fseek(fp_mtx3, 0L, SEEK_SET);
+    if (sz == total_num_node * sizeof(INDEXTYPE)) {
+      fread((void*)pntrb, sizeof(INDEXTYPE), total_num_node, fp_mtx3);
+    }
+    else {
+        printf("size of file(%s) is wrong\n", filename);
+        return 0;
+    }
+    fclose(fp_mtx3);
+  }
+  else {
+    printf("Cannot find %s\n", filename);
     return 0;
   }
 
@@ -174,23 +259,31 @@ int main(int argc, char **argv) {
   //printf("pntrb[1] = %lu\n" ,pntrb[1]);
   //printf("pntrb[2] = %lu\n" ,pntrb[2]);
   //printf("pntrb[3] = %lu\n" ,pntrb[3]);
+  // ===============================================================================//
 
+  // ===============================================================================//
   // pntre from file
-  FILE* fp_mtx3 = fopen("../dataset/graph/harvard/harvard_pntre.dat", "rb");
+  strcpy(filename, dataset_path);
+  strcat(filename, "_pntre.dat");
+  printf("file name = %s\n", filename);
+  FILE* fp_mtx4 = fopen(filename, "rb");
+  INDEXTYPE* pntre = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  total_num_node * sizeof(INDEXTYPE));
 
-  INDEXTYPE* pntre = (INDEXTYPE*) aligned_alloc(CACHE_LINE_SIZE,  file_size_pntr_hvd * sizeof(INDEXTYPE));
-
-  if (fp_mtx3 != NULL) {
-    fseek(fp_mtx3, 0L, SEEK_END);
-    uint64_t sz = ftell(fp_mtx3);
-    fseek(fp_mtx3, 0L, SEEK_SET);
-    if (sz == file_size_pntr_hvd * sizeof(INDEXTYPE)) {
-      fread((void*)pntre, sizeof(INDEXTYPE), file_size_pntr_hvd, fp_mtx3);
+  if (fp_mtx4 != NULL) {
+    fseek(fp_mtx4, 0L, SEEK_END);
+    uint64_t sz = ftell(fp_mtx4);
+    fseek(fp_mtx4, 0L, SEEK_SET);
+    if (sz == total_num_node * sizeof(INDEXTYPE)) {
+      fread((void*)pntre, sizeof(INDEXTYPE), total_num_node, fp_mtx4);
     }
-    fclose(fp_mtx3);
+    else {
+        printf("size of file(%s) is wrong\n", filename);
+        return 0;
+    }
+    fclose(fp_mtx4);
   }
   else {
-    printf("Cannot find harvard_pntre.dat\n");
+    printf("Cannot find %s\n", filename);
     return 0;
   }
 
@@ -198,38 +291,18 @@ int main(int argc, char **argv) {
   //printf("pntre[1] = %lu\n" ,pntre[1]);
   //printf("pntre[2] = %lu\n" ,pntre[2]);
   //printf("pntre[3] = %lu\n" ,pntre[3]);
+  // ===============================================================================//
 
-  // b alloc
-  VALUETYPE* b = (VALUETYPE*) aligned_alloc(CACHE_LINE_SIZE,  num_node*dim_vector_hvd * sizeof(VALUETYPE));
-  INDEXTYPE ldb = dim_vector_hvd;
 
-  // b from file
-  FILE* fp_mtx4 = fopen("../dataset/graph/harvard/harvard_b_mat.dat", "rb");
-
-  if (fp_mtx4 != NULL) {
-    fseek(fp_mtx4, 0L, SEEK_END);
-    uint64_t sz = ftell(fp_mtx4);
-    fseek(fp_mtx4, 0L, SEEK_SET);
-    if (sz == num_node*dim_vector_hvd * sizeof(VALUETYPE)) {
-      fread((void*)b, sizeof(VALUETYPE), num_node*dim_vector_hvd, fp_mtx4);
-    }
-    fclose(fp_mtx4);
-  }
-  else {
-    printf("Cannot find harvard_b_mat.dat\n");
-    return 0;
-  }
-
-  //printf("b[0] = %f\n" ,b[0]);
-  //printf("b[1] = %f\n" ,b[1]);
-  //printf("b[2] = %f\n" ,b[2]);
-  //printf("b[3] = %f\n" ,b[3]);
-
+  // ===============================================================================//
   // val alloc
   VALUETYPE* val = (VALUETYPE*) aligned_alloc(CACHE_LINE_SIZE,  nonzero * sizeof(VALUETYPE));
   
   // val from file
-  FILE* fp_mtx5 = fopen("../dataset/graph/harvard/harvard_val.dat", "rb");
+  strcpy(filename, dataset_path);
+  strcat(filename, "_val.dat");
+  printf("file name = %s\n", filename);
+  FILE* fp_mtx5 = fopen(filename, "rb");
 
   if (fp_mtx5 != NULL) {
     fseek(fp_mtx5, 0L, SEEK_END);
@@ -238,10 +311,14 @@ int main(int argc, char **argv) {
     if (sz == nonzero * sizeof(VALUETYPE)) {
       fread((void*)val, sizeof(VALUETYPE), nonzero, fp_mtx5);
     }
+    else {
+        printf("size of file(%s) is wrong\n", filename);
+        return 0;
+    }
     fclose(fp_mtx5);
   }
   else {
-    printf("Cannot find harvard_val.dat\n");
+    printf("Cannot find %s\n", filename);
     return 0;
   }
 
@@ -249,14 +326,15 @@ int main(int argc, char **argv) {
   //printf("val[1] = %f\n" ,val[1]);
   //printf("val[2] = %f\n" ,val[2]);
   //printf("val[3] = %f\n" ,val[3]);
+  // ===============================================================================//
 
   // c alloc
-  VALUETYPE* c = (VALUETYPE*) aligned_alloc(CACHE_LINE_SIZE,  num_node*dim_vector_hvd * sizeof(VALUETYPE));
-  INDEXTYPE ldc = dim_vector_hvd;
+  VALUETYPE* c = (VALUETYPE*) aligned_alloc(CACHE_LINE_SIZE,  total_num_node*num_dim * sizeof(VALUETYPE));
+  INDEXTYPE ldc = num_dim;
 
   // m, k
-  INDEXTYPE m = num_node;      // rows of A 
-  INDEXTYPE k = dim_vector_hvd;// dimension: col of A and B
+  INDEXTYPE m = total_num_node; // rows of A 
+  INDEXTYPE k = num_dim;	    // dimension: col of A and B
 
 #ifdef GEM_FORGE
   gf_detail_sim_start();
